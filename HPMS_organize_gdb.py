327385b64b9940f9b2da523ba6952337f49f3a59
mrfay <mrfay@uvm.edu>
2024-02-22 22:09:40 -0500

    finished gdb organization sccript for HPMS prep input

--- HPMS_organize_gdb.py
+++ HPMS_organize_gdb.py
@@ -21,14 +21,9 @@ print('''
 
 # import packages 
 import arcpy
-import geopandas as gpd
 import pandas as pd
-import pyogrio
-import pyarrow
-import time
-import datetime
-import sys
 import os
+import sys
 
 # overwrite existing files (False = No, True = Yes)
 arcpy.env.overwriteOutput = True
@@ -44,12 +39,11 @@ gdb_folder_path = "C:/Users/mrfay/OneDrive - University of Vermont/Documents/LOC
 # name project gdb
 gdb_name = "HPMS_test.gdb"
 
-# file path to raw HPMS gdb
-HPMS_path = "Z:/ROAD_AQ/HPMS Traffic Data/Downloaded Data/NTAD2019_GDB_HPMS2018_2019_10_21/NTAD2019_GDB_HPMS2018_2019_10_21.gdb"
-
+# file path to raw data geodatabases
+HPMS_source_gdb = "Z:/ROAD_AQ/HPMS Traffic Data/Downloaded Data/NTAD2019_GDB_HPMS2018_2019_10_21/NTAD2019_GDB_HPMS2018_2019_10_21.gdb"
 # file paths to the raw Census shapefiles
-census_county_path = "Z:/ROAD_AQ/HPMS Traffic Data/Downloaded Data/2020 Census County Boundaries/tl_2020_us_county.shp"
-census_uac_path = "Z:/ROAD_AQ/HPMS Traffic Data/Downloaded Data/2020 Census Urban AC Boundaries/tl_rd22_us_uac20.shp"
+county_source_gdb = "Z:/ROAD_AQ/HPMS Traffic Data/Census Boundaries/2020_Census_County_Boundaries.gdb"
+uac_source_gdb = "Z:/ROAD_AQ/HPMS Traffic Data/Census Boundaries/2010_Census_Urban_AC_Boundaries.gdb"
 
 # ----- LOAD DATA --------------------------------------------------------------
 print("Loading data...")
@@ -58,7 +52,7 @@ print("Loading data...")
 pd.set_option('display.float_format', '{:.4f}'.format)
 
 # check that all file paths exist
-file_paths = [gdb_folder_path, HPMS_path, census_county_path, census_uac_path]
+file_paths = [gdb_folder_path, HPMS_source_gdb, county_source_gdb, uac_source_gdb]
 
 for file_path in file_paths:
     if not os.path.exists(file_path):
@@ -67,43 +61,50 @@ for file_path in file_paths:
     else: 
         print(f"File or directory exists: {file_path}") 
 
-# Load data using geopandas
-try: 
-    # load raw HPMS feature classes
-    data_hpms_123 = gpd.read_file(HPMS_path, layer='hpms_2018_fsys123_2019_10_21', engine="pyogrio", use_arrow=True)
-    data_hpms_456 = gpd.read_file(HPMS_path, layer='hpms_2018_fsys456_2019_10_21', engine="pyogrio", use_arrow=True)
-    print("HPMS data loaded")
-    # load input census county data
-    data_county = gpd.read_file(census_county_path, engine="pyogrio", use_arrow=True)
-    print("census county geometry loaded")
-    data_uac = gpd.read_file(census_uac_path, engine="pyogrio", use_arrow=True)
-    print("census urban area codes geometry loaded")
-except Exception as e:
-    print(f"ERROR: The data could not be loaded. {e}")
-    print(f"ERROR: The script will terminate.")
-    sys.exit()
-
-
 # ----- START ----- # 
+
+# >>> Create new file geodatabase <<<
 
 # use the CreateFileGDB_management tool to create the new project geodatabase
 arcpy.CreateFileGDB_management(gdb_folder_path, gdb_name)
+print(f"Geodatabase {gdb_name} created.")
+HPMS_target_gdb = os.path.join(gdb_folder_path, gdb_name)
+arcpy.env.workspace = HPMS_target_gdb     
 
-# List of GeoDataFrames and their corresponding names in the geodatabase
-data_list = [(data_hpms_123, 'hpms_2018_fsys123_2019_10_21'), 
-             (data_hpms_456, 'hpms_2018_fsys456_2019_10_21'), 
-             (data_county, 'US_census_county_2020'), 
-             (data_uac, 'US_census_uac_2020')]
+# >>> Copy HPMS feature classes to the new geodatabase <<<
 
-# Path to the geodatabase
-gdb_path = os.path.join(gdb_folder_path, gdb_name)
+print("Copying HPMS data to geodatabase...")
 
-# Copy each GeoDataFrame to the geodatabase
-for data, name in data_list:
-    try:
-        # Convert GeoDataFrame to a feature class
-        arcpy.CopyFeatures_management(data, os.path.join(gdb_path, name))
-        print(f"Feature class {name} copied to geodatabase.")
-    except Exception as e:
-        print(f"ERROR: The feature class could not be copied. {e}")
+# Feature class names in the source and target geodatabases
+fc_123 = "hpms_2018_fsys123_2019_10_21"
+fc_456 = "hpms_2018_fsys456_2019_10_21"
 
+# Copy the feature classes
+try:
+    arcpy.Copy_management(f"{HPMS_source_gdb}\\{fc_123}", f"{HPMS_target_gdb}\\{fc_123}")
+    arcpy.Copy_management(f"{HPMS_source_gdb}\\{fc_456}", f"{HPMS_target_gdb}\\{fc_456}")
+    print("HPMS data copied to geodatabase.")
+except Exception as e:
+    print(f"ERROR: The feature class could not be copied. {e}")
+
+# >>> Copy Census feature class to the new geodatabase <<<
+
+print("Copying Census data to geodatabase...")
+
+# Feature class names in the source and target geodatabases
+fc_county = "tl_2020_us_county"
+fc_cnty_new = "US_census_county_2020"
+fc_uac = "tl_2020_us_uac10"
+fc_uac_new = "US_census_uac_2010"
+
+# Copy the feature classes
+try:
+    arcpy.Copy_management(f"{county_source_gdb}\\{fc_county}", f"{HPMS_target_gdb}\\{fc_cnty_new}")
+    arcpy.Copy_management(f"{uac_source_gdb}\\{fc_uac}", f"{HPMS_target_gdb}\\{fc_uac_new}")
+    print("HPMS data copied to geodatabase.")
+except Exception as e:
+    print(f"ERROR: The feature classes could not be copied. {e}")
+
+
+print("Census data copied to geodatabase.")    
+print('----- END OF SCRIPT -----')
