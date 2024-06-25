28f778492ca2c038b53a2d15515548bf8854155b
Aviral Chawla <avi.champ@hotmail.com>
2024-06-25 14:00:08 -0400

    senstivity scripts and results

---
+++ scripts/misc/run_senstivity_hdv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --partition=bluemoon
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem=30G
+#SBATCH --time=30:00:00
+#SBATCH --job-name="hdv_sentivity"
+source ~/.bash_profile
+
+echo "Starting sbatch script myscript.sh at:`date`"
+echo "  running host:    ${SLURMD_NODENAME}"
+echo "  assigned nodes:  ${SLURM_JOB_NODELIST}"
+echo "  partition used:  ${SLURM_JOB_PARTITION}"
+echo "  jobid:           ${SLURM_JOBID}"
+
+conda activate hpms
+module load gcc/10.5.0
+
+python senstivity_hdv.py --run $1
---
+++ scripts/misc/senstivity_mdv.py
@@ -0,0 +1,78 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import sys
+import seaborn as sns
+import geopandas as gpd
+import tqdm
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description="# of Run")
+parser.add_argument('--run', type=int, help='Run number')
+args = parser.parse_args()
+
+run = args.run
+
+print("Run number: ", run)
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+plt.rcParams['axes.axisbelow'] = True
+
+parent_dir = str(Path(__file__).resolve().parent.parent)
+
+# Add the parent directory to sys.path
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+# Now you can import the module as if it was in the same directory
+import utils.aadt_predictor as ap
+
+NUM_JOBS = int(os.getenv('SLURM_NTASKS')) if os.getenv('SLURM_NTASKS') else -1
+
+### Load data
+HPMS_DIR = Path('../../data/processed_data/HPMS')
+RANDOM_STATE = None
+RESPONSE_VARS = ['AADT_MDV', 'AADT_HDV']
+RF_PREDICTOR_VARS = ["STATEFP", "COUNTYFP", "F_SYSTEM", "THROUGH_LANES", "AADT"]
+
+predictor = ap.AADTPredictor(HPMS_DIR / 'hpms_aadt_subset.csv', None, random_state = RANDOM_STATE)
+ERROR_SCOPE = np.concatenate([np.arange(0, 0.01, 0.001), np.arange(0.01, 0.1, 0.005)])
+results_mdv_perturb = []
+
+response_var = RESPONSE_VARS[0]
+
+predictor.response_var = response_var
+predictor.subset_train_data()
+
+for error in tqdm.tqdm(ERROR_SCOPE):
+        
+    predictor.split_data(RF_PREDICTOR_VARS, state_fips= None, test_size=0.2)
+    predictor.initialize_model('Random Forest', n_jobs = NUM_JOBS)
+
+    # perturb total AADT
+    predictor.y_train = predictor.y_train * (1 +  np.random.uniform(-error, error, predictor.X_train.shape[0]))
+    predictor.y_test = predictor.y_test * (1 +  np.random.uniform(-error, error, predictor.X_test.shape[0]))
+
+    predictor.fit_model()
+
+    y_pred = predictor.model.predict(predictor.X_test)
+
+    r2 = r2_score(predictor.y_test, y_pred)
+    mse = mean_squared_error(predictor.y_test, y_pred)
+    mae = mean_absolute_error(predictor.y_test, y_pred)
+
+    results_mdv_perturb.append({
+        'response_var': response_var,
+        'error': error,
+        'r2': r2,
+        'rmse': np.sqrt(mse),
+        'mae': mae,
+        'run': run
+    })
+
+results_mdv_perturb_df = pd.DataFrame(results_mdv_perturb)
+results_mdv_perturb_df.to_csv(f'../../data/results/sensitivity_results_MDV_perturb_{run}.csv', index = False)
